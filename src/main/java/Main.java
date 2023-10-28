2b8edeebe60b82674cb1c998e6312d57c080ad4b
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-10-28 13:17:28 +0300

    Program example

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import core.basesyntax.operation.*;
+import model.DailyReport;
+import model.FruitTransaction;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    static FileService<FruitTransaction>  fileServiceCsv = new FileServiceCsvImpl();
+    static OperationStrategy operationStrategy;
+    public static void main(String[] args) {
+        Map<Operation, OperationHandler> operationOperationHandlerMap = new HashMap<>();
+        operationOperationHandlerMap.put(Operation.BALANCE, new BalanceOperationHandler());
+        operationOperationHandlerMap.put(Operation.SUPPLY, new SupplyOperationHandler());
+        operationOperationHandlerMap.put(Operation.PURCHASE, new PurchaseOperationHandler());
+        operationOperationHandlerMap.put(Operation.RETURN, new ReturnOperationHandler());
+        operationStrategy = new OperationStrategyImpl(operationOperationHandlerMap);
+        List<FruitTransaction> listOfTransactions = fileServiceCsv.read("test.csv");
+        Map<String, Integer> map = new HashMap<>();
+        for (FruitTransaction transaction : listOfTransactions) {
+            OperationHandler operationHandler = operationStrategy.get(transaction.operation());
+            operationHandler.perform(transaction.fruit(), transaction.quantity(), map);
+        }
+        DailyReport report = new DailyReport(map);
+        fileServiceCsv.write("result.csv", report.createDailyReportString());
+
+    }
+}
